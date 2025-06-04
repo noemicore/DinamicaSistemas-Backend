59188c626929c6d5ada1837b8e640335dc9c1587
LUZZAPARAN <lmzaparanc@ufpso.edu.co>
2025-06-03 23:34:47 -0500

    "cambios bd"

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
   user: process.env.PGUSER || 'postgres',
   host: process.env.PGHOST || 'localhost',
   database: process.env.PGDATABASE || 'postgres',
-  password: process.env.PGPASSWORD || '1234',
+  password: process.env.PGPASSWORD || '12345',
   port: process.env.PGPORT ? parseInt(process.env.PGPORT) : 5432,
 });
 
