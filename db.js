9e31489bd0b2c8a2906cfc49415bf1228b0f959a
Noemi Collantes <noemicollantereyes24@gmail.com>
2025-06-06 13:04:21 -0500

    navbar final

--- db.js
+++ db.js
@@ -5,8 +5,8 @@ const pool = new Pool({
   user: process.env.PGUSER || 'postgres',
   host: process.env.PGHOST || 'localhost',
   database: process.env.PGDATABASE || 'postgres',
-  password: process.env.PGPASSWORD || '12345',
-  port: process.env.PGPORT ? parseInt(process.env.PGPORT) : 5432,
+  password: process.env.PGPASSWORD || '123456',
+  port: process.env.PGPORT ? parseInt(process.env.PGPORT) : 5433,
 });
 
 module.exports = pool;
